03699402d8e8d7944dd25d636ec30d9db2d5648c
Mantas <Mantas@Mantass-MacBook-Pro.local>
2013-02-18 17:32:39 +0200

    Created first app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+FirstApp::Application.config.secret_token = '30f328928f41f8b56f8d348564243e51d6af4ab0e2ac5017fade6ae74562da78566a760907cfe2147e73cecf9216e8cb623acedb90ba766c471bfd2b3dd8913e'
